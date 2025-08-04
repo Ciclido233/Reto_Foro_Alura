01a2d866a951994add4c1d2e28b76d9f49103dff
Angelo Kevin García Hernández <kevingarciasce@gmail.com>
2025-08-04 17:48:19 -0600

    Update JwtService.java

--- src/main/java/com/alura/forokev/security/JwtService.java
+++ src/main/java/com/alura/forokev/security/JwtService.java
@@ -6,10 +6,10 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails; // Estaba incompleto
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.security.Key; // Estaba mal escrito como "key"
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
